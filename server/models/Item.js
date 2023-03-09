cf617847d57bc6ee019c4785d25b75f4dd04c142
Addison Philips <addyphilips66@gmail.com>
2023-03-09 14:11:42 +0000

    tests working and some of react working

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,21 +1,13 @@
 // const {Sequelize} = require('sequelize')
 const {sequelize, DataTypes} = require('../db')
 // const sauce = require('../routes/sauces')
-const Item = sequelize.define('item', {
-<<<<<<< HEAD
-    name: Sequelize.STRING,
-    description: Sequelize.STRING,
-    price: Sequelize.NUMBER,
-    category: Sequelize.STRING,
-    image: Sequelize.STRING
-=======
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.NUMBER,
-    category: DataTypes.STRING,
-    image: DataTypes.STRING
->>>>>>> c2f6989d0a9906cb44a24e281890c8be1e01c13b
-})
+const Item = sequelize.define("item", {
+  title: DataTypes.STRING,
+  description: DataTypes.STRING,
+  price: DataTypes.NUMBER,
+  category: DataTypes.STRING,
+  image: DataTypes.STRING,
+});
 
 
 
